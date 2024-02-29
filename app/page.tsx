a5c6a878fe7b4330cea904838d4bb1dee8e0e7a2
masonomara <mason.omara@gmail.com>
2024-02-29 17:18:36 -0500

    fixed up logo

--- app/page.tsx
+++ app/page.tsx
@@ -13,22 +13,54 @@ export default function Home() {
         controls={false}
         className={styles.video}
       />
-      
 
       <div className={styles.content}>
         <div className={styles.titleWrapper}>
           <div className={styles.logo}>
-            <Image src={"public/oss.png"} alt={"OSS"} fill />
+            <Image src={"/oss.png"} alt={"OSS"} fill />
           </div>
           <h1 className={styles.logoTitle}>
             OpenSource
             <br />
             Solutions
           </h1>
-          {/* <p>
-            Software solutions company available for work March 2024. Scope
-            includes design and development of apps, websites, e-commerce.
-          </p> */}
+          <p className={styles.subtitle}>
+            Innovative software
+            <br />
+            solutions available for
+            <br />
+            work March 2024.
+          </p>
+          <div className={styles.desktopTitle}>
+            <h1 className={styles.logoTitle}>OpenSource Solutions</h1>
+            <p className={styles.subtitle}>
+              Innovative software solutions available for work March 2024.
+            </p>
+          </div>
+        </div>
+        <div className={styles.infoWrapper}>
+          <div className={styles.infoContainer}>
+            <p className={styles.infoTitle}>Contact</p>
+            <div className={styles.infoLink}>Email Us &gt;</div>
+            <p className={styles.infoSub}>
+              Portfolio available
+             
+              by request.
+            </p>
+          </div>
+          <div className={styles.infoContainer}>
+            <p className={styles.infoTitle}>Focuses</p>
+
+            <p className={styles.infoMain}>
+              Software Development
+              <br />
+              User Experience Design
+              <br />
+              Mobile and Web Apps
+              <br />
+              E-Commerce Solutions
+            </p>
+          </div>
         </div>
       </div>
     </main>
